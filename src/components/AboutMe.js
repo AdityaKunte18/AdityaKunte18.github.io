bc97ea9c39a3b5bcb7acd015e76ee17ab3be7eb3
AdityaKunte18 <akunte2@illinois.edu>
2025-09-05 14:09:14 -0500

    updated about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,11 +4,13 @@ function AboutMe() {
   return (
     <div className="AboutMeContainer">
       <p className="aboutMeText">
-        I am currently a <span className="highlightedText">senior</span> in <span className="highlightedText">Computer Science</span> at the <span className="highlightedText"> University of Illinois at Urbana-Champaign (UIUC)</span>. 
+        I am currently a <span className="highlightedText">Master's student</span> in <span className="highlightedText">Computer Science</span> at the <span className="highlightedText"> University of Illinois at Urbana-Champaign (UIUC)</span>, where I also did my <span className="highlightedText"> Bachelor's in Computer Science, with a minor in Statistics. </span> 
         <br/><br/>
-        Right now, I am experimenting with different fields like <span className="highlightedText">systems-level programming</span> and <span className="highlightedText">machine-learning</span>.
+        Right now, I'm learning how to develop <span className="highlightedText">systems for AI</span> and <span className="highlightedText">AI for systems.</span> This means understanding how to <span className="highlightedText"> train, and deploy machine learning models </span> <span className="highlightedText"> at scale, cost-effectively.</span> I build tools and pipelines powered by <span className="highlightedText"> agentic frameworks</span> like <span className="highlightedText">Langraph, LLamaIndex, and n8n.</span>
         <br/><br/>
-        I have also had some experience in <span className="highlightedText">embedded systems</span> (soldering and programming microcontrollers at hackathons) as well as <span className="highlightedText">full-stack dev</span> work (building applications for Android using <span className="highlightedText">Android Studio</span> and <span className="highlightedText">React Native</span> and building websites in <span className="highlightedText">Next.js</span> and <span className="highlightedText">Bootstrap</span>)
+        I'm also an experienced <span className="highlightedText">full-stack developer.</span>  I've built Android Applications using <span className="highlightedText">Android Studio</span> and <span className="highlightedText">React Native</span>, and I also build websites in <span className="highlightedText">Node.js (React, Next)</span> with backend technologies like <span className="highlightedText">Postgres, Flask, and Firebase.</span>
+        <br/><br/>
+
       </p>
     </div>
   );
