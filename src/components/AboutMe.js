7eafb5d99bf6d5925d22032b9a50745e88bfec5f
AdityaKunte18 <akunte2@illinois.edu>
2025-09-05 16:05:47 -0500

    fixed build error issues

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,13 +4,12 @@ function AboutMe() {
   return (
     <div className="AboutMeContainer">
       <p className="aboutMeText">
-        I am currently a <span className="highlightedText">Master's student</span> in <span className="highlightedText">Computer Science</span> at the <span className="highlightedText"> University of Illinois at Urbana-Champaign (UIUC)</span>, where I also did my <span className="highlightedText"> Bachelor's in Computer Science, with a minor in Statistics. </span> 
-        <br/><br/>
-        Right now, I'm learning how to develop <span className="highlightedText">systems for AI</span> and <span className="highlightedText">AI for systems.</span> This means understanding how to <span className="highlightedText"> train, and deploy machine learning models </span> <span className="highlightedText"> at scale, cost-effectively.</span> I build tools and pipelines powered by <span className="highlightedText"> agentic frameworks</span> like <span className="highlightedText">Langraph, LLamaIndex, and n8n.</span>
-        <br/><br/>
-        I'm also an experienced <span className="highlightedText">full-stack developer.</span>  I've built Android Applications using <span className="highlightedText">Android Studio</span> and <span className="highlightedText">React Native</span>, and I also build websites in <span className="highlightedText">Node.js (React, Next)</span> with backend technologies like <span className="highlightedText">Postgres, Flask, and Firebase.</span>
-        <br/><br/>
-
+        I am currently a <span className="highlightedText">Master&apos;s student</span> in <span className="highlightedText">Computer Science</span> at the <span className="highlightedText">University of Illinois at Urbana-Champaign (UIUC)</span>, where I also did my <span className="highlightedText">Bachelor&apos;s in Computer Science, with a minor in Statistics.</span>
+        <br /><br />
+        Right now, I&apos;m learning how to develop <span className="highlightedText">systems for AI</span> and <span className="highlightedText">AI for systems.</span> This means understanding how to <span className="highlightedText">train, and deploy machine learning models</span> <span className="highlightedText">at scale, cost-effectively.</span> I build tools and pipelines powered by <span className="highlightedText">agentic frameworks</span> like <span className="highlightedText">Langraph, LLamaIndex, and n8n.</span>
+        <br /><br />
+        I&apos;m also an experienced <span className="highlightedText">full-stack developer.</span> I&apos;ve built Android Applications using <span className="highlightedText">Android Studio</span> and <span className="highlightedText">React Native</span>, and I also build websites in <span className="highlightedText">Node.js (React, Next)</span> with backend technologies like <span className="highlightedText">Postgres, Flask, and Firebase.</span>
+        <br /><br />
       </p>
     </div>
   );
